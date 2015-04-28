f09c80bd764638559f8010a53f14c62fd43522b9
Mikael Karon <mikael@karon.se>
2015-04-28 18:04:45 +0800

    Scaffolding

---
+++ test/main-test.js
@@ -0,0 +1,12 @@
+define([
+  "buster",
+  "../main"
+], function (buster, Router) {
+  "use strict";
+
+  var assert = buster.referee.assert;
+  var refute = buster.referee.refute;
+
+  buster.testCase("mu-route/main", {
+  });
+});
